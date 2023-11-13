c2b41f75b4f1b860df6b913800e0abc3a1676d44
EoinFirz03 <G00418325@atu.ie>
2023-11-13 09:29:34 +0000

    adding task 1

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,7 +1,10 @@
 package ie.atu;
 
+import java.util.scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("please enter a number between 0-9 :");
+        Scsnner myscan = new Scanner(system.in);
     }
 }
