be876ef0dba4f03771fdbce42b246b6ce1172d2e
EoinFirz03 <G00418325@atu.ie>
2023-11-13 10:02:17 +0000

    adding while loop in

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -5,27 +5,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("please enter a number between 0-9 :");
-        Scanner myscan = new Scanner(System.in);
 
-        try {
-            //Read user input as a string
-            String input = myscan.nextLine();
+        int x = 0;
 
-            // parse the input to an integer
-            int myNum = Integer.parseInt(input);
+        while(x<1){
+            System.out.println("please enter a number between 0-9 :");
+            Scanner myscan = new Scanner(System.in);
 
-            // checkmif the number is between 0 abd 9
-            if (myNum >= 0 && myNum <= 9) {
-                System.out.println("you entered " + myNum);
-            } else {
+
+            // adding in try
+            try {
+                //Read user input as a string
+                String input = myscan.nextLine();
+
+                // parse the input to an integer
+                int myNum = Integer.parseInt(input);
+
+                // check if the number is between 0 abd 9
+                if (myNum >= 0 && myNum <= 9) {
+                    System.out.println("you entered " + myNum);
+                    x++;
+                } else {
+                    System.out.println("not a valid number ");
+                }
+            }
+
+            //adding in catch
+            catch(NumberFormatException e){
                 System.out.println("not a valid number ");
             }
+
         }
 
-        catch(NumberFormatException e){
-            System.out.println("please enter a valid number ");
-        }
+
 
     }
 }
