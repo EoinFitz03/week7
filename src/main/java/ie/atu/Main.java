8d5d73a87f08657b78ee43dd87f123480d962616
EoinFirz03 <G00418325@atu.ie>
2023-11-13 09:35:03 +0000

    adding task 1

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,10 +1,28 @@
 package ie.atu;
 
-import java.util.scanner;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("please enter a number between 0-9 :");
-        Scsnner myscan = new Scanner(system.in);
+        Scanner myscan = new Scanner(System.in);
+
+        //Read user input as a string
+        String input = myscan.nextLine();
+
+        // parse the input to an integer
+        int myNum = Integer.parseInt(input);
+
+        // checkmif the number is between 0 abd 9
+        if (myNum >= 0 && myNum <=9){
+            System.out.println("you entered "+ myNum );
+        }
+
+        else{
+            System.out.println("not a valid number ");
+        }
+
+
+
     }
 }
